a182db012506c7ec04f29002d91e01c788861952
Alex Shen <xs90@duke.edu>
2025-02-08 14:53:46 -0500

    add rider logic

---
+++ docker-deploy/duber-web-app/rider/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import request_ride, get_eta, rider_dashboard, edit_ride, cancel_ride, RideDetailView
+
+urlpatterns = [
+    path("request/", request_ride, name="request_ride"),
+    path("dashboard/", rider_dashboard, name="rider_dashboard"),
+    path("edit/<int:ride_id>/", edit_ride, name="edit_ride"),  
+    path("get_eta/", get_eta, name="get_eta"),
+    # Other URL patterns
+    path('cancel_ride/<int:ride_id>/', cancel_ride, name='cancel_ride'),
+    path('ride/<int:pk>/', RideDetailView.as_view(), name='ride_detail'),
+]
