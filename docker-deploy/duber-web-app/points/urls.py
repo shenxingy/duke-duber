e0d758b40c67f8d2bf757604837109240d90d2bb
Matty-7 <jh730@duke.edu>
2025-02-09 11:15:56 -0500

    added succulent and fresh produce options

--- docker-deploy/duber-web-app/points/urls.py
+++ docker-deploy/duber-web-app/points/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import my_points, redeem_tree
+from .views import my_points, redeem_tree, redeem_succulent, redeem_produce
 
 app_name = "points"
 
 urlpatterns = [
     path('my-points/', my_points, name='my_points'),
     path('redeem-tree/', redeem_tree, name='redeem_tree'),
+    path('redeem-succulent/', redeem_succulent, name='redeem_succulent'),
+    path('redeem-produce/', redeem_produce, name='redeem_produce'),
 ]
--- docker-deploy/duber-web-app/points/views.py
+++ docker-deploy/duber-web-app/points/views.py
@@ -5,6 +5,8 @@ from django.shortcuts import redirect
 from django.contrib import messages    
 
 TREE_COST = 1000  # Assume 1000 points to plant a tree
+SUCCULENT_COST = 300  # 多肉植物兑换成本
+PRODUCE_COST = 500  # 蔬果兑换成本
 
 @login_required
 def redeem_tree(request):
@@ -21,6 +23,42 @@ def redeem_tree(request):
         messages.success(request, "Congratulations! You planted a tree in Duke Forest!")
     else:
         messages.error(request, "Not enough points to redeem a tree.")
+
+    return redirect('points:my_points')
+
+@login_required
+def redeem_succulent(request):
+    user_points = UserPoints.objects.get(user=request.user)
+
+    if user_points.points >= SUCCULENT_COST:
+        user_points.points -= SUCCULENT_COST
+        user_points.save()
+
+        PointsTransaction.objects.create(
+            user=request.user, points=SUCCULENT_COST, transaction_type='redeem'
+        )
+
+        messages.success(request, "Congratulations! You got a succulent from Duke Garden!")
+    else:
+        messages.error(request, "Not enough points to redeem a succulent.")
+
+    return redirect('points:my_points')
+
+@login_required
+def redeem_produce(request):
+    user_points = UserPoints.objects.get(user=request.user)
+
+    if user_points.points >= PRODUCE_COST:
+        user_points.points -= PRODUCE_COST
+        user_points.save()
+
+        PointsTransaction.objects.create(
+            user=request.user, points=PRODUCE_COST, transaction_type='redeem'
+        )
+
+        messages.success(request, "Congratulations! You got fresh produce from Duke Farm!")
+    else:
+        messages.error(request, "Not enough points to redeem produce.")
 
     return redirect('points:my_points')
 
